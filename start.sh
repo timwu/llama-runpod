75eaf01649cb5c1b88e721b4da395d10f906c15e
Sam Hartman <hartmans@debian.org>
2025-02-22 11:57:31 -0700

    Update for where runpod-volume ends up

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set
+set -e
+test -d /runpod-volume &&cd /runpod-volume
+
 if [ -n "$MODEL_URL" ]; then
     if [ -d model ]; then
         (cd model&&git pull)
@@ -7,4 +9,4 @@ if [ -n "$MODEL_URL" ]; then
         git clone $MODEL_URL model
     fi
 fi
-exec python3  handle.py
+exec python3  /workspace/handle.py
