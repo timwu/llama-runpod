c7b85f50fca37f92ffa3121bb8fba04ccb3baee3
Tim Wu <tim.wu.0@gmail.com>
2025-03-04 22:28:51 -0800

    Drop openai_route from handler

--- workspace/handle.py
+++ workspace/handle.py
@@ -12,6 +12,11 @@ llm = Llama(**args)
 
 def handler(event):
     inp = event["input"]
+    
+    # Scratch the openai_api route to allow direct access
+    if "openai_route" in inp:
+        del inp["openai_route"]
+
     return llm(**inp)
 
 
